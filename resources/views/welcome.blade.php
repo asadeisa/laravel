091859efd9fda23db6ac404b2969e4cf2f174327
asadeisa <asadeisa232@gmail.com>
2021-10-22 20:56:50 +0300

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+
+<script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyCmcggvhqetUo4r1aam6KoOy2lc8EDN-Go",
+      authDomain: "realtime-28da0.firebaseapp.com",
+      projectId: "realtime-28da0",
+      storageBucket: "realtime-28da0.appspot.com",
+      messagingSenderId: "463740966808",
+      appId: "1:463740966808:web:e0561e77f1eb5256467ded",
+      measurementId: "G-KMZJHJXB64"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  </script>
+
+  {{-- server key 
+    	AAAAa_kdl5g:APA91bHpvvk2I0IyRrU-vD8IhctCu3WKEtyQeSrdHEsqTnP-nd_SnmhNxkDp_hbDdPpCprmG-jDIb1oSx1Yhvjs6WeuE0WVoFO61JyY_TbfQLQ4WvPs3i1i05IOFKEneraq530Sfrdtw 
+    --}}
+
+    {{-- Sender ID
+        463740966808
+        
+        --}}
+
